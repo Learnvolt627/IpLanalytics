4b3c28f0b44ce43803ffd428248747664b1afe3d
Yashvardhan Singh Rathore <yashvardhan.rathore.ug24@nsut.ac.in>
2026-06-30 00:27:29 +0530

    checked calculations and analysed data

--- explore.py
+++ explore.py
@@ -16,7 +16,15 @@ df=pd.read_csv("IPL.csv")
 # sample=df[df['match_id']==real_id]
 # print('i' ,sample[['innings','over','ball','runs_batter','runs_total','runs_target']].head(10))
 
-second_innings =df[(df['match_id']==335982)& (df['innings']==2)]
-print("rows in 2nd innings;", len(second_innings))
-print(second_innings[['innings','over','ball','runs_batter','runs_total','runs_target']].head(10))
-print(second_innings[['innings','over','ball','runs_batter','runs_total','runs_target']].tail(10))
+# second_innings =df[(df['match_id']==335982)& (df['innings']==2)]
+# print("rows in 2nd innings;", len(second_innings))
+# print(second_innings[['innings','over','ball','runs_batter','runs_total','runs_target']].head(10))
+# print(second_innings[['innings','over','ball','runs_batter','runs_total','runs_target']].tail(10))
+
+sample= df[(df['match_id']==335982) & (df['innings']==2)]
+cols = ['over','ball','ball_no','valid_ball','runs_total','wicket_kind','player_out','striker_out']
+print(sample[cols].head(10))
+
+print("--every wicket that feel in this innings--")
+fell = sample[sample['player_out'].notna()]
+print(fell[['over','ball','ball_no','player_out','wicket_kind','striker_out']])
