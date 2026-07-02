5ee738b1e17d9a9084270c43b9305c7c69affa47
Yashvardhan Singh Rathore <yashvardhan.rathore.ug24@nsut.ac.in>
2026-07-02 23:15:02 +0530

    model trained

--- train_model.py
+++ train_model.py
@@ -5,10 +5,10 @@ df=pd.read_csv("win_probability_dataset.csv")
 train= df[df['season']<=2022]
 test= df[df['season']>=2023]
 
-print(f"Train rows: {len(train)}")
-print(f"Test rows: {len(test)}")
-print(f"Train seasons: {sorted(train['season'].unique())}")
-print(f"Test seasons: {sorted(test['season'].unique())}")
+# print(f"Train rows: {len(train)}")
+# print(f"Test rows: {len(test)}")
+# print(f"Train seasons: {sorted(train['season'].unique())}")
+# print(f"Test seasons: {sorted(test['season'].unique())}")
 
 num_features=['current_score', 'wickets_in_hand', 'balls_remaining', 'runs_needed'
               ,'crr', 'rrr']
@@ -20,22 +20,26 @@ Y_train= train['label']
 X_test= test[num_features+cat_features]
 Y_test=test['label']
 
-print(X_train.shape)
-print(X_train.head(3))
+# print(X_train.shape)
+# print(X_train.head(3))
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
 
 pre = ColumnTransformer([
-    ('cat', OneHotEncoder(handle_unknown='ignore'), cat_features)
-], remainder='passthrough')
+    ('cat', OneHotEncoder(handle_unknown='ignore'), cat_features),
+    ('num', StandardScaler(), num_features)
+])
 
 model= Pipeline([
     ('prep',pre),
     ('clif', LogisticRegression(max_iter=1000))
 ])
 
-print("Pipline built successfully")
+# print("Pipline built successfully")
+
+model.fit(X_train, Y_train)
+print("Model trained")
